0dd2e60e322ccdce755cc73c68ce83990f488e18
castellanprime <okusanya.damilola@gmail.com>
2016-10-17 19:25:45 -0500

    TODO: Compute random AI move

---
+++ games/AIOthello.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+
+"""
+	This module contains the implementation of a board and 
+	gamestate.
+
+	File: rec_othello_ai.py
+	Date: Oct 15, 2016
+	Author: Okusanya David
+"""
+import sys
+import re
+import os
+import random
+from OthelloUtils import GameState, Board
+
+class Othello:
+
+	# Representation of the players on the board
+	__PLAYER_BLACK = 'b'
+	__PLAYER_WHITE = 'w'
+
+	# Status codes
+	ERROR = "invalid move"
+	__NO_MOVE = "no moves available"
+	__SUCCESS = "valid move"
+	__GAME_END = "Game end"
+
+	def __init__(self):
+		self.board = Board(self.__PLAYER_BLACK, self.__PLAYER_WHITE)
+		self.board.h_setPosition(self.__PLAYER_WHITE, (4, 4))
+		self.board.h_setPosition(self.__PLAYER_WHITE, (5, 5))
+		self.board.h_setPosition(self.__PLAYER_BLACK, (4, 5))
+		self.board.h_setPosition(self.__PLAYER_BLACK, (5, 4))
+		self.state = GameState(self.__PLAYER_BLACK, self.board)
+
+	# Making the class variables immutable
+	@property
+	def PLAYER_BLACK(self):
+		return type(self).__PLAYER_BLACK    
+
+	@property
+	def PLAYER_WHITE(self):
+		return type(self).__PLAYER_WHITE   
+
+
+	def __validate_position(self, position):
+		""" Checks to see if supplied position is correct 
+			
+			Args:
+				position: A tuple (row, column) of the player's position
+			Raises:
+				ValueError
+		"""
+		if position not in self.state.board.validpositions:
+			raise ValueError("Position not valid")
+
+	def place(self, position_to_move, player_opp=None):
+		""" Places a player's piece on the board
+
+		Args:
+			player: A character representing a player
+			position_to_move: A tuple (row, column) the player wants to move to
+		Return:
+			status message: Error message if the piece can not be played. If the piece can be played, the 
+		        method returns a __SUCCESS message.
+
+		"""
+
+		player = self.state.turn
+		opp_player = ''
+		if player_opp == None:
+			opp_player = self.get_opp_player(player)[0]
+		else:
+			opp_player = player_opp
+
+		# If player has legal moves	
+		if self.state.next_moves:
+			print("Legal moves for ", player, " = ", self.state.next_moves)
+			if position_to_move not in self.state.next_moves:
+				return "\nPlayer {0}'s move:{1} Return_code:{2}\n".format(player, position_to_move, self.ERROR)
+			self.flip_opp_player_positions(player, opp_player, position_to_move)
+			self.state = GameState(opp_player, self.state.board)
+		return "\nPlayer {0}'s move:{1} Return_code:{2}\n".format(player, position_to_move, self.__SUCCESS)
+
+		# If player does not have legal moves
+		if player_opp != None:
+			self.state = Gamestate(opp_player, self.state.board)
+			if self.state.next_moves:
+				print("Legal moves for ", opp_player, " = ", self.state.next_moves)
+				if position_to_move not in self.state.next_moves:
+					return "\nPlayer {0}'s move:{1} Return_code:{2}\n".format(player, position_to_move, self.ERROR)
+				self.flip_opp_player_positions(player, opp_player, position_to_move)
+				self.state = GameState(opp_player, self.state.board)
+			return "\nPlayer {0}'s move:{1} Return_code:{2}\n".format(player, position_to_move, self.__SUCCESS)
+
+		return "\n Player {0} has {1} \n".format(player, self.__NO_MOVE)
+
+	def get_opp_player(self, player):
+		""" Returns the opposite player in the music set
+
+		Args:
+			player: A character representing a player
+		Return:
+			opp_player: A list containing the opp_player
+
+		"""
+		s = [self.__PLAYER_BLACK, self.__PLAYER_WHITE]
+		opp_player = list(set(s).difference([player]))
+		return opp_player
+
+
+	def flip_opp_player_positions(self, player, opp_player, position_to_move):
+		""" Flips all the pieces for a particular players' move
+
+		Args:
+			position: a tuple indicating the row and column to start the search
+			player: character representing player
+		"""
+
+		l = self.state.board.getFlips(player, opp_player, position_to_move)
+		print(opp_player, "'s positions to flip = " , l)
+		self.state.board.h_setPosition(player, position_to_move)
+		for value in l:
+			self.state.board.h_setPosition(player, value)
+			self.state.board.removePiece(opp_player, value)  
+
+	def toString(self):
+		""" Print the representation of the board to console"""
+		return self.state.board.__str__()
+
+	def prompt(self):
+		""" Header about the game rules in a prompt message"""
+
+		print("Welcome to Othello!! Have fun!!")
+		print("=" * 10)
+		print("\n\n")
+		print("Rules for game \n\n", 
+		" 1. Black is represented by 'b' and White is represented by 'w' \n", 
+		" 2. Black plays first \n", 
+		" 3. If a player has no legal moves, the other player plays\n ", 
+		" 4. If there are no more legal moves for the two players, game ends.\n", 
+		" 5. The player with the maximum number of pieces wins or else there is a draw \n")
+
+	def get_input(self):
+		""" User input from the command line 
+
+		Args:
+			input_player: Default value is None. A character representing the player
+		Return:
+			input_player: A character representing the player
+			input_position: A tuple representing the row and col to place player's piece
+		Raise:
+			ValueError
+		"""
+		input_position = ()
+		try:
+			str = "{0} plays now".format(self.state.turn)
+			print(str)
+			temp_input = input("Enter your position(row, col) (ex: 4 3): ")
+			input_position = tuple(map(int, temp_input.split(' ')))
+			self.__validate_position(input_position)
+		except ValueError as e:
+			print(e)
+		except KeyboardInterrupt:
+			print("\nInterrupted\n")
+			try :
+				sys.exit(0)
+			except SystemExit:
+				os._exit(0)
+		return input_position
+    
+	def human_play(self):
+
+		position = ()
+
+		while True:
+			print(self.state.board.__str__())
+
+			# First input
+			position = self.get_input()									
+
+			# If first input is wrong
+			while len(position) == 0:
+				position = self.get_input()
+
+			# First place							
+			status_message = self.place(position)							
+
+			# If move is not in self.state.moves
+			while len(re.findall(r'invalid move', status_message)) > 0:	
+				print(status_message)		
+				position = self.get_input()
+				status_message = self.place(position) 
+
+			# If there are not legal moves
+			if len(re.findall(r'no moves available', status_message)) > 0:
+				print(status_message)		
+				print("Entering legal moves")
+				# Get the opposite player to play
+				opp_player = self.get_opp_player(self.state.turn)				
+				status_message = self.place(position, opp_player)
+				# If oppposite player does not have legal moves
+				if len(re.findall(r'no moves available', status_message)) > 0:
+					print(self.state.board.win_or_lose())
+					print(self.__GAME_END)
+					return
+
+			# If the game is finished
+			if self.state.board.get_no_of_spaces() == 0:
+				print(self.state.board.win_or_lose())
+				print(self.__GAME_END)
+				return
+
+def main_loop():
+	o = Othello()
+	o.prompt()
+	o.human_play()
+
+if __name__ == '__main__':
+	main_loop()
---
+++ games/OthelloUtils.py
@@ -0,0 +1,503 @@
+#!/usr/bin/env python
+
+"""
+	This module contains the implementation of a board and 
+	gamestate.
+
+	File: OthelloUtils.py
+	Date: Oct 10, 2016
+	Author: Okusanya David
+"""
+
+import copy
+
+class GameState:
+	""" State object """
+
+
+	# Global weights for SEF
+	
+	w_flip_num = 2				# weight attached to the number of flipped pieces
+	w_corner_num = 1000 		# weight attached to the number of corner held by a player
+	w_edge_num = 30				# weight attached to the number of edges held by a player
+	w_corner_adj_num = -100		# weight attached to the corner adjacent positions held by the player
+	#w_empty_adj_num = 20		# weight attached to the empty spaces related to the number
+
+	def __init__(self, player, board, computer= False, minimax=False, weights=None):
+		self.board = board
+		self.turn = player
+		self.next_moves = self.board.getLegalMoves(self.turn)
+		self.minimax_move = None
+		if weights:
+			self.changeWeights(weights)
+		if minimax:
+			self.sef = SEF(self.board)
+		else:
+			self.sef = 0
+
+	def SEF(self, board):
+		""" This computes the SEF for a particular board from the following values
+
+		Args:
+			board: A gameboard
+		Return:
+			utility: A value that indicates how good the position is
+		"""
+		flip_num = self.board.getNoOfFlippedPieces(self.turn)
+		corner_num = self.board.getNoOfCornerPieces(self.turn)
+		edge_num = self.board.getNoOfEdgePieces(self.turn)
+		corner_adj_num = self.board.getNoOfCornerAdjacentPieces(self.turn)
+		#empty_adj_num = self.board.getNoOfEmptyAdjNum(self.turn)
+
+		return (w_flip_num * flip_num) + (w_corner_num * corner_num) + (w_edge_num * edge_num) + (w_corner_adj_num * corner_adj_num) 
+
+	def setMinimaxMove(self, move):
+		self.minmax_move = move
+
+	def changeWeights(weights):
+		for key, value in weights.items():
+			if key == 'w_corner_num':
+				w_corner_num = value
+			elif key == 'w_edge_num':
+				w_edge_num = value
+			elif key == 'w_flip_num':
+				w_flip_num = value
+			elif key == 'w_corner_adj_num':
+				w_corner_adj_num = value
+
+
+class Board:    
+
+	# Gameboard: A board of 8 x 8 squares
+	# The board has two extra rows and columns to make checking for bounds
+	gameboard = [['-' for n in range(10)] for n in range(10)]
+
+	def __init__(self, player_one, player_two):
+		self.setUp(player_one, player_two)
+
+	def setUp(self, player_one, player_two):
+		""" Initial setup script"""
+		for row, value in enumerate(self.gameboard):
+			value[0] = "*"
+			value[-1] = "*"
+		self.gameboard[0] = ["*" for n in range(len(self.gameboard[0]))]
+		self.gameboard[-1] = ["*" for n in range(len(self.gameboard[-1]))]
+		self.white_curr_pos = []
+		self.black_curr_pos = []
+		self.validpositions = [(row,col) for row in range(1, 9) for col in range(1, 9)]
+		self.edges = [(3, 1), (4, 1), (5, 1), (6, 1) , (8, 1),
+			(1, 3),(1, 4), (1, 5), (1, 6), (3, 8), (4, 8), (5, 8), 
+			(6, 8), (8, 3), (8, 4), (8, 5), (8, 6)]
+		self.corners = [(1, 1),(8, 8), (8, 1), (1, 8)]
+		self.corner_adj_positions = [(1, 2), (2, 1), (2, 2), (1, 7), (2, 7), (2, 8), (7, 1),
+			(8, 2), (7, 2), (7, 8), (8, 7), (7, 7)]
+		self.player_one = player_one
+		self.player_two = player_two
+
+	def h_setPosition(self, player, position):
+		""" Helper function for to actually place the player on the board
+
+			Args:
+				player: A character indicating the colour of the player  
+			position: A tuple indicating the particular cell to place the player in
+		"""
+		self.gameboard[position[0]][position[1]] = player
+		if player == self.player_one:
+			self.black_curr_pos.append((position[0],position[1]))
+		elif player == self.player_two:
+			self.white_curr_pos.append((position[0],position[1]))
+
+	def removePiece(self, player, position):
+		""" Removes a piece from the board 
+
+			Args:
+				player: A character indicating the colour of the player  
+				position: A tuple indicating the particular cell to place the player in 
+		"""
+		if player == self.player_one:
+			self.black_curr_pos.remove(position)
+		elif player == self.player_two:
+			self.white_curr_pos.remove(position)
+
+
+	def getNoOfFlippedPieces(self, player):
+		""" Gets number of flipped pieces
+
+			Args:
+				player: A character indicating the colour of the player
+			Returns:
+				value: An integer number
+		"""
+		if player == self.player_one:
+			return len(self.black_curr_pos)
+		elif player == self.player_two:
+			return len(self.white_curr_pos)
+
+	def getNoOfCornerPieces(self, player):
+		""" Gets number of corner pieces
+
+			Args:
+				player: A character indicating the colour of the player
+			Returns:
+				value: An integer number 
+		"""
+		if player == self.player_one:
+			return len([val for val in self.black_curr_pos if val in self.corners])
+		elif player == self.player_two:
+			return len([val for val in self.white_curr_pos if val in self.corners])
+
+	def getNoOfEdgePieces(self,player):
+		""" Gets number of edges pieces
+
+			Args:
+				player: A character indicating the colour of the player
+			Returns:
+				value: An integer number 
+		"""
+
+		if player == self.player_one:
+			return len([val for val in self.black_curr_pos if val in self.edges])
+		elif player == self.player_two:
+			return len([val for val in self.white_curr_pos if val in self.edges])
+
+	def getNoOfCornerAdjacentPieces(self, player):
+		""" Gets number of corner adjacent pieces
+
+			Args:
+				player: A character indicating the colour of the player
+			Returns:
+				value: An integer number 
+		"""
+
+		if player == self.player_one:
+			return len([val for val in self.black_curr_pos if val in self.corner_adj_positions])
+		elif player == self.player_two:
+			return len([val for val in self.white_curr_pos if val in self.corner_adj_positions])
+
+	def allPositions(self, player):
+		""" Returns a list of all places for a particular player 
+
+			Args:
+				player: character representing player
+			Return:
+				list: a list of tuples representing player positions
+
+		"""
+		#results = []
+		if player == self.player_one:
+			return self.black_curr_pos
+		elif player == self.player_two:
+			return self.white_curr_pos
+
+	def get_no_of_spaces(self):
+		return(64 - (len(self.black_curr_pos) + len(self.white_curr_pos)))
+
+	def getMoves(self, position, player):
+		"""
+			This helper method returns a list of moves for a given position. The algorithm goes thus:
+
+			While the number of columns or rows have not been exceeded:
+				if position has execceded the bounds of the board
+					break
+				if there is a space adjacent to the position on the path of the search
+					break
+				if there is a space on the path of search and it is not preceeded by the same colour as the 
+				given position
+					add that to the moves list 
+
+			Args:
+				position: a tuple indicating the row and column to start the search
+				player: character representing player
+
+			Returns:
+				moves: a list of legal positions or moves
+		"""
+
+		moves = []
+		row, col = position
+
+
+		# row-i, col + i
+		for i in range(1, 9):
+			if col + i > 8 and row - i < 0:
+				break
+			if i == 1 and (self.gameboard[row-i][col+i] == '-' or self.gameboard[row-i][col+i] == "*"):
+				break 
+			if self.gameboard[row-i][col+i] == '-' and (self.gameboard[(row-i)+1][col+(i-1)] != player and self.gameboard[(row-i)+1][col+(i-1)] != '-'):
+				moves.append((row-i, col+i))
+				#print(player, " You have appended me ", (row-i, col+i))
+				break
+
+		# row, col + i
+		for i in range(1, 9):
+			if col + i > 8:
+				break
+			if i == 1 and (self.gameboard[row][col+i] == '-' or self.gameboard[row][col+i] == "*"):
+				break 
+			if self.gameboard[row][col+i] == '-' and (self.gameboard[row][col+(i-1)] != player and self.gameboard[row][col+(i-1)] != '-'): 
+				moves.append((row, col+i))
+				#print(player, " You have appended me ", (row, col+i))
+				break
+
+		# row-i, col - i
+		for i in range(1, 9):
+			if col - i < 0 or row - i < 0:
+				break
+			if i == 1 and (self.gameboard[row-i][col-i] == '-' or self.gameboard[row-i][col-i] == "*"):
+				break 
+			if self.gameboard[row-i][col-i] == '-' and (self.gameboard[(row-i)+1][(col-i)+1] != player and self.gameboard[(row-i)+1][(col-i)+1] != '-') : 
+				moves.append((row-i, col-i))
+				#print(player, " You have appended me ", (row-i, col-i))
+				break
+
+		# row+i, col + i
+		for i in range(1, 9):
+			if col + i > 8 or  row + i > 8:
+				break
+			if i == 1 and (self.gameboard[row+i][col+i] == '-' or self.gameboard[row+i][col+i] == "*"):
+				break 
+			if self.gameboard[row+i][col+i] == '-' and (self.gameboard[row+(i-1)][col+(i-1)] != player and self.gameboard[row+(i-1)][col+(i-1)] != '-'):
+				moves.append((row+i, col+i))
+				#print(player, " You have appended me ", (row+i, col+i))
+				break
+
+		# row-i, col
+		for i in range(1, 9):
+			if row - i < 0:
+				break
+			if i == 1 and (self.gameboard[row-i][col] == '-' or self.gameboard[row-i][col] == "*"):
+				break 
+			if self.gameboard[row-i][col] == '-' and (self.gameboard[(row-i)+1][col] != player and self.gameboard[(row-i)+1][col] != '-'):
+				moves.append((row-i, col))
+				#print(player, " You have appended me ", (row-i, col))
+				break
+
+		# row, col - i
+		for i in range(1, 9):
+			if col - i < 0:
+				break
+			if i == 1 and (self.gameboard[row][col-i] == '-' or self.gameboard[row][col-i] == "*"):
+				break 
+			if self.gameboard[row][col-i] == '-' and (self.gameboard[row][(col-i)+1] != player and self.gameboard[row][(col-i)+1] != '='):
+				moves.append((row, col-i))
+				#print(player, " You have appended me ", (row, col-i))
+				break
+
+		# row + i, col  
+		for i in range(1, 9):
+			if row + i > 8:
+				break
+			if i == 1 and (self.gameboard[row+i][col] == '-' or self.gameboard[row+i][col] == "*"):
+				break 
+			if self.gameboard[row+i][col] == '-' and (self.gameboard[row+(i-1)][col] != player and self.gameboard[row+(i-1)][col] != '-'):
+				moves.append((row+i, col))
+				#print(player, " You have appended me ", (row+i, col))
+				break
+
+		# row + i, col - i  
+		for i in range(1, 9):
+			if col - i < 0 or row + i > 8:
+				break
+			if i == 1 and (self.gameboard[row+i][col-i] == '-' or self.gameboard[row+i][col-i] == "*"):
+				break 
+			if self.gameboard[row+i][col-i] == '-' and (self.gameboard[row+(i-1)][(col-i)+1] != player and self.gameboard[row+(i-1)][(col - i)+1] != '-'): 
+				moves.append((row+i, col-i))
+				#print(player, " You have appended me ", (row+i, col-i))
+				break
+
+		return moves
+
+
+	def getFlips(self, player, opp_player, position_to_move):
+
+		""" Return the opposing player positions to flip
+
+			Args:
+				player: character representing player
+				opp_player: character representing the opposing player
+				position_to_move: A tuple indicating the particular cell to flip
+			Return:
+				flips: A list of opp_player positions to flip
+
+		"""
+
+		flips = []
+		row, col = position_to_move[0], position_to_move[1]
+		flag = False    # Flag indicates that we can stop
+		count = 0
+
+		# row - i, col - i
+		for i in range(1, 9):
+			if row - i < 0 or col - i < 0:
+				break
+			if i == 1 and (self.gameboard[row-i][col-i]== player or self.gameboard[row-i][col-i] == '-'):
+				break
+			if self.gameboard[row-i][col-i] == player and self.gameboard[row-(i-1)][col-(i-1)] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row-i, col-i))
+			flag = False
+
+		# row - i , col
+		for i in range(1, 9):
+			if row - i < 0:
+				break
+			if i == 1 and (self.gameboard[row-i][col]== player or self.gameboard[row-i][col] == '-'):
+				break
+			if self.gameboard[row-i][col] == player and self.gameboard[row-(i-1)][col] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row-i, col))
+			flag = False
+
+		# row - i , col + i
+		for i in range(1, 9):
+			if row - i < 0 or col + i > 8:
+				break
+			if i == 1 and (self.gameboard[row-i][col + i]== player or self.gameboard[row-i][col+i] == '-'):
+				break
+			if self.gameboard[row-i][col+i] == player and self.gameboard[row-(i-1)][col+(i-1)] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row-i, col+i))
+			flag = False
+
+		# row , col + i
+		for i in range(1, 9):
+			if col + i > 8:
+				break
+			if i == 1 and (self.gameboard[row][col + i]== player or self.gameboard[row][col+i] == '-'):
+				break
+			if self.gameboard[row][col+i] == player and self.gameboard[row][col+(i-1)] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row, col+i))
+			flag = False
+
+		# row + i , col + i
+		for i in range(1, 9):
+			if row + i > 8  or col + i > 8:
+				break
+			if i == 1 and (self.gameboard[row+i][col + i]== player or self.gameboard[row+i][col+i] == '-'):
+				break
+			if self.gameboard[row+i][col+i] == player and self.gameboard[row+(i-1)][col+(i-1)] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row+i, col+i))
+			flag = False
+
+		# row+i , col
+		for i in range(1, 9):
+			if row + i > 8:
+				break
+			if i == 1 and (self.gameboard[row+i][col]== player or self.gameboard[row+i][col] == '-'):
+				break
+			if self.gameboard[row+i][col] == player and self.gameboard[row+(i-1)][col] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row+i, col))
+			flag = False
+
+		# row+i , col - i
+		for i in range(1, 9):
+			if row + i > 8 or col - i < 0:
+				break
+			if i == 1 and (self.gameboard[row+i][col - i]== player or self.gameboard[row+i][col-i] == '-'):
+				break
+			if self.gameboard[row+i][col-i] == player and self.gameboard[row+(i-1)][col-(i-1)] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row+i, col-i))
+			flag = False
+
+		# row , col - i
+		for i in range(1, 9):
+			if col - i < 0:
+				break
+			if i == 1 and (self.gameboard[row][col - i]== player or self.gameboard[row][col-i] == '-'):
+				break
+			if self.gameboard[row][col-i] == player and self.gameboard[row][col-(i-1)] == opp_player:
+				flag = True
+				count = i
+				break
+
+		if flag:
+			for i in range(1, count):
+				flips.append((row, col-i))
+			flag = False
+
+		return flips
+
+
+	def getLegalMoves(self, player):
+		""" Get the legal moves for a player
+
+			Args:
+				player: A character representing a player
+			Return:
+				results_list: A list of legal positions that the player can be placed at. Else a query_list is returned
+			            if there are no legal moves
+		"""
+
+		query_list = self.allPositions(player)
+		print(player, "('s) currently occupied positions = ", query_list)
+		if len(query_list) != 0:
+			results_list = []
+			for position in query_list:
+				results_list.append(self.getMoves(position, player))
+			results_list = [value[n] for idx, value in enumerate(results_list) for n in range(len(value))]
+			#print("Nice job = ", results_list)
+			results_list[:] = [results_list[i] for i in range(len(results_list)) if i == results_list.index(results_list[i])] 
+			return results_list
+		return query_list  
+
+	def win_or_lose(self):
+		""" Determines who wins the game"""
+
+		if len(self.white_curr_pos) > len(self.black_curr_pos):
+			return "{0} wins.He/she has {1} pieces - {2} pieces".format(self.player_two, len(self.white_curr_pos), len(self.black_curr_pos))
+		elif len(white_curr_pos) < len(black_curr_pos):
+			return "{0} wins.He/she has {1} pieces - {2} pieces".format(self.player_one, len(self.black_curr_pos), len(self.white_curr_pos))
+		else:
+			return "Draw. Neither {0} nor {1} wins!!".format(self.player_one, self.player_two)
+
+	def __str__(self):
+		string = "  "
+		for i in range(1, 9):
+			string += "".join([" ", str(i)])
+		string += "\n"
+
+		print_list = self.gameboard[1: -1]
+		for i, value in enumerate(print_list, 1):
+			print_l = [["|", value[j]] for j in range(1, len(value) - 1)]
+			print_l[:] = [val[n] for val in print_l for n in range(len(val))]
+			string += "".join([str(i), " ", "".join(str(e) for e in print_l), "|", "\n"])
+		return string
